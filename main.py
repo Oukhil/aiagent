25a87cbc5afdaa1f3ed9c14fa1a73625fa354429
Odrot <oukhil@gmail.com>
2026-03-07 00:36:07 +0200

    Token metadata print

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
+    if response.usage_metadata is None:
+        raise RuntimeError("Failed API request")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}" )
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response: ")
     print(response.text)
 
