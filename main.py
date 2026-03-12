ce805c2ce77891b5b31b5be0e088e1720228e3d4
Odrot <oukhil@gmail.com>
2026-03-12 13:16:14 +0200

    Adding verbose flag

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -12,17 +13,23 @@ def main():
     
     parser = argparse.ArgumentParser(description="AI Agent")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=args.user_prompt
+        model='gemini-2.5-flash', contents=messages
     )
     if response.usage_metadata is None:
         raise RuntimeError("Failed API request")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}" )
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}" )
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print("Response: ")
     print(response.text)
 
